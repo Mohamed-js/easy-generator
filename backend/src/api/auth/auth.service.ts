26f28761d6ea82bbfb6a8ccbc51734cd733f777f
Mohamed Atef <atefhamosa@gmail.com>
2025-03-06 06:38:08 +0200

    Fix auth service

--- backend/src/api/auth/auth.service.ts
+++ backend/src/api/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { compare } from 'bcrypt';
@@ -8,61 +12,51 @@ import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
 import { ConfigService } from '@nestjs/config';
 
-
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectModel(User.name) private userModel: Model<UserDocument>,
-        private jwtService: JwtService,
-        private configService: ConfigService,
-    ) { }
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
-    async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
-        const { email, name, password } = signUpDto;
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { email, name, password } = signUpDto;
 
-        const existingUser = await this.userModel.findOne({ email });
-        if (existingUser) {
-            throw new BadRequestException('Email already registered');
-        }
-
-        const user = await this.userModel.create({ email, name, password });
-        const token: string = this.generateToken(user._id)
-        return { token };
+    const existingUser = await this.userModel.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestException('Email already registered');
     }
 
+    const user = await this.userModel.create({ email, name, password });
+    const token: string = this.generateToken(user._id);
+    return { token };
+  }
 
-    async signIn(signInDto: SignInDto): Promise<{ token: string }> {
-        const { email, password } = signInDto;
+  async signIn(signInDto: SignInDto): Promise<{ token: string }> {
+    const { email, password } = signInDto;
 
-        const user = await this.userModel.findOne({ email });
-        if (!user) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-
-        const isPasswordValid: boolean = await compare(password, user.password);
-        if (!isPasswordValid) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-
-        const token: string = this.generateToken(user._id)
-        return { token };
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
     }
 
-    async getMe(userId: Types.ObjectId): Promise<{ user: UserDocument | null }> {
-        const user = await this.userModel.findById(userId).select('_id email name');
-
-        return { user };
+    const isPasswordValid: boolean = await compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
     }
 
-    private generateToken(userId: Types.ObjectId): string {
-        // const jwtSecret = this.configService.get<string>('JWT_SECRET');
-        // if (!jwtSecret) {
-        //     throw new Error('JWT_SECRET is not defined');
-        // }
-        // return this.jwtService.sign({ userId }, {
-        //     secret: jwtSecret,
-        //     expiresIn: '1d',
-        // });
-        return this.jwtService.sign({ userId });
-    }
+    const token: string = this.generateToken(user._id);
+    return { token };
+  }
+
+  async getMe(userId: Types.ObjectId): Promise<{ user: UserDocument | null }> {
+    const user = await this.userModel.findById(userId).select('_id email name');
+
+    return { user };
+  }
+
+  private generateToken(userId: Types.ObjectId): string {
+    return this.jwtService.sign({ userId });
+  }
 }
